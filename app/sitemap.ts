3070fc9a2a0f5b7db629c68090b1ee7cc1014096
marmutMerahJambu <119766108+rammm2005@users.noreply.github.com>
2025-09-12 21:51:33 +0800

    fix: add sitemap fix on URL

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,63 +1,34 @@
 import { MetadataRoute } from "next";
 
 const projects = [
-  {
-    title: "Population Management Mobile Application",
-  },
-  {
-    title: "E-commerce Platform for Semmaga Business",
-  },
-  {
-    title: "Static Travel websites in Indonesia",
-  },
-  {
-    title: "Portfolio Website Templates",
-  },
-  {
-    title: "Tictactoe Game that",
-  },
-  {
-    title: "Kill the Bird in the Air",
-  },
-  {
-    title: "Data Visualization from Spreadsheet",
-  },
-  {
-    title: "Freelancer Static Website gets First Winner on ITCC 2023",
-  },
-  {
-    title: "Modern Eccommers Static Website get Third Winner on IDB Campus",
-  },
-  {
-    title: "Modern Daily Me Tracking App for Young people to keep a healthy life",
-  },
-  {
-    title: "3DES Encryption Decryption Algorithm with Python and Telegram Bot",
-  },
+    "population-management-mobile-application",
+    "e-commerce-platform-for-semmaga-business",
+    "static-travel-websites-in-indonesia",
+    "portfolio-website-templates",
+    "tictactoe-game-that",
+    "kill-the-bird-in-the-air",
+    "data-visualization-from-spreadsheet",
+    "freelancer-static-website-gets-first-winner-on-itcc-2023",
+    "modern-eccommers-static-website-get-third-winner-on-idb-campus",
+    "modern-daily-me-tracking-app-for-young-people-to-keep-a-healthy-life",
+    "3des-encryption-decryption-algorithm-with-python-and-telegram-bot",
 ];
 
-function slugify(title: string) {
-  return title
-    .toLowerCase()
-    .replace(/\s+/g, "-") // spasi jadi -
-    .replace(/[^\w-]+/g, ""); // buang karakter aneh
-}
+export default function sitemap(): MetadataRoute.Sitemap {
+    const domain = "https://www.rama-dev.tech";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const domain = process.env.DOMAIN || "https://www.rama-dev.tech";
-
-  return [
-    {
-      url: domain,
-      lastModified: new Date(),
-    },
-    {
-      url: `${domain}/projects`,
-      lastModified: new Date(),
-    },
-    ...projects.map((p) => ({
-      url: `${domain}/projects/${slugify(p.title)}`,
-      lastModified: new Date(),
-    })),
-  ];
+    return [
+        {
+            url: domain,
+            lastModified: new Date(),
+        },
+        {
+            url: `${domain}/projects`,
+            lastModified: new Date(),
+        },
+        ...projects.map((slug) => ({
+            url: `${domain}/projects/${slug}`,
+            lastModified: new Date(),
+        })),
+    ];
 }
