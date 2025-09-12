7ee66693d154d0b9704b3f8c7b83d42636c36054
marmutMerahJambu <119766108+rammm2005@users.noreply.github.com>
2025-09-12 21:25:49 +0800

    fix: sitemap for good SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,63 @@
 import { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const domain = process.env.DOMAIN || "https://rama-dev.tech/";
-    return [
+const projects = [
+  {
+    title: "Population Management Mobile Application",
+  },
+  {
+    title: "E-commerce Platform for Semmaga Business",
+  },
+  {
+    title: "Static Travel websites in Indonesia",
+  },
+  {
+    title: "Portfolio Website Templates",
+  },
+  {
+    title: "Tictactoe Game that",
+  },
+  {
+    title: "Kill the Bird in the Air",
+  },
+  {
+    title: "Data Visualization from Spreadsheet",
+  },
+  {
+    title: "Freelancer Static Website gets First Winner on ITCC 2023",
+  },
+  {
+    title: "Modern Eccommers Static Website get Third Winner on IDB Campus",
+  },
+  {
+    title: "Modern Daily Me Tracking App for Young people to keep a healthy life",
+  },
+  {
+    title: "3DES Encryption Decryption Algorithm with Python and Telegram Bot",
+  },
+];
 
-        {
-            url: domain,
-            lastModified: new Date(),
-        },
-        {
-            url: `${domain}/projects/{slug}`,
-            lastModified: new Date(),
-        },
-    ];
+function slugify(title: string) {
+  return title
+    .toLowerCase()
+    .replace(/\s+/g, "-") // spasi jadi -
+    .replace(/[^\w-]+/g, ""); // buang karakter aneh
+}
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const domain = process.env.DOMAIN || "https://rama-dev.tech";
+
+  return [
+    {
+      url: domain,
+      lastModified: new Date(),
+    },
+    {
+      url: `${domain}/projects`,
+      lastModified: new Date(),
+    },
+    ...projects.map((p) => ({
+      url: `${domain}/projects/${slugify(p.title)}`,
+      lastModified: new Date(),
+    })),
+  ];
 }
